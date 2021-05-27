3603850697a2bae00ad211fbc1dbfc7474ce5b13
alirezasyed <codingchallenge2020@yahoo.com>
2021-05-27 01:27:01 -0700

    change le nom de localhost

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 <!-- Connection à la base de donnée (Début)-->
   <?php
   try {
-    $bdd = new PDO('mysql:host=localhost;dbname=catalogueacs;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO('mysql:host=127.0.0.1;dbname=catalogueacs;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
   }
     catch(PDOException $e)
   {
